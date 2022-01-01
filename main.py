dca622f0b90138287877929ffb6157d15c440800
Lauren <kerk0100@mylaurier.ca>
2022-01-01 14:28:49 -0500

    testing flask

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import urllib3
 import json
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
+from flask import request
+
+
 auth_url = "https://www.strava.com/oauth/token"
 activites_url = "https://www.strava.com/api/v3/athlete/activities"
 
@@ -20,8 +23,6 @@ access_token = res.json()['access_token']
 print("Access Token = {}\n".format(access_token))
 
 header = {'Authorization': 'Bearer ' + access_token}
-# param = {'per_page': 200, 'page': 6}
-# activity_data = requests.get(activites_url, headers=header, params=param).json()
 
 i = 1
 
