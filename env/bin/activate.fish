66eae8d5ee36fe51517c771afea54a831ae7a5ce
Lauren <kerk0100@mylaurier.ca>
2022-01-02 15:11:05 -0500

    adding a filter for pace

--- env/bin/activate.fish
+++ env/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/laurenkerker/Documents/test/env"
+set -gx VIRTUAL_ENV "/Users/laurenkerker/Documents/strava_analysis/env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
