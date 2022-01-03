4a0592cae369c769e2849fc663dcb1e94c6b897a
Lauren <kerk0100@mylaurier.ca>
2022-01-03 15:27:18 -0500

    formatting metrics to view

--- static/js/script.js
+++ static/js/script.js
@@ -4,16 +4,3 @@ form.addEventListener('submit', (e) => {
   e.preventDefault();
   return false;
 });
-
-var expanded = false;
-
-$(function() {
-
-  $('#chkveg').multiselect({
-    includeSelectAllOption: true
-  });
-
-  $('#btnget').click(function() {
-    alert($('#chkveg').val());
-  });
-});
