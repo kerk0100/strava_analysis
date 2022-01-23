b20bddce5c88b432648e739d7df2a96ba6c012a9
Lauren <kerk0100@mylaurier.ca>
2022-01-23 12:11:18 -0800

    working on merge feature

--- load_data.py
+++ load_data.py
@@ -1,4 +1,5 @@
 # Access refresh token and extracting data
+from re import A
 import requests
 import urllib3
 import json
@@ -11,9 +12,10 @@ import pandas as pd
 import time
 import plotly.express as px
 import plotly.io as pio
-import plotly
+# import plotly
 from plotly.offline import plot
-from plotly.graph_objs import Scatter
+# from plotly.graph_objs import Scatter
+from pathlib import Path
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
@@ -24,7 +26,7 @@ csv.field_size_limit(sys.maxsize)
 def load(id,secret,refresh):
 
     auth_url = "https://www.strava.com/oauth/token"
-    activites_url = "https://www.strava.com/api/v3/athlete/activities"
+    activites_url = "https://www.strava.com/api/v3/athlete/activities/"
     payload = {
         'client_id': str(id),
         'client_secret': str(secret),
@@ -51,7 +53,11 @@ def load(id,secret,refresh):
     # save all activities to txt file (note: do to pickle, the file is unreadable)
     with open('static/data/data.txt', 'wb') as f:
         pickle.dump(activity_data, f)
+    with open('static/data/data.csv', 'w') as f:
+        writer = csv.writer(f)
+        writer.writerow(activity_data)
     return activity_data
+
 
 # -----------------------------   
 # Helper Functions
@@ -163,15 +169,11 @@ def distance(dist, type, operand, activity_data, metrics, start_date, end_date):
     return df
 
 # ----------------------------- 
-
-# trying a different way to filter data
-# loads data into df, then filters to build graph
-def graph_data(year_list):
-    pio.templates.default = "plotly_white"
+def get_data():
     # load activities from txt file
     with open('static/data/data.txt', 'rb') as f:
         activities = pickle.load(f)
-    column_names = ["id","name", "type", "distance", "month", "year", "total_elevation_gain"]
+    column_names = ["id","name", "type", "distance", "month", "year", "total_elevation_gain", "has_heartrate", "average_heartrate", "average_speed"]
     df = pd.DataFrame(columns = column_names)
 
     # iterate through each page of activities
@@ -190,39 +192,122 @@ def graph_data(year_list):
                     # save distance in km not m
                     dist = i["distance"] / 1000
                     metric_list.append(dist)
+                elif m == "average_heartrate":
+                    if i["has_heartrate"] == False:
+                        avg_hr = 0
+                    else:
+                        avg_hr = i["average_heartrate"]
+                    metric_list.append(avg_hr)
                 else:
                     metric_list.append(i[m])
             df.loc[df.shape[0]] = metric_list
+    return df
+
+# trying a different way to filter data
+# loads data into df, then filters to build graph
+def graph_data(year_list, a_type):
+    pio.templates.default = "plotly_white"
+    data_file = Path("static/data/graph_df.csv")
+    if data_file.exists() == False:
+        df = get_data()
+        df.to_csv("static/data/graph_df.csv", index=False)
+        months = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"]
+    else:
+        df = pd.read_csv("static/data/graph_df.csv")
+        months = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"]
 
     df_t = df[["year", "month", "type", "distance"]]
-    df_line = df_t[(df_t["type"] == "Run")]
+    if a_type != "All":
+        df_line = df_t[(df_t["type"] == a_type)]
+    else:
+        df_line = df_t
+    df_line = df_line.astype({"year": str, "month": str})
     df_graph = pd.DataFrame(columns = ["year", "month", "distance"])
     # years that user specified (graphs.html)
     years = year_list
-    months = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"]
     for y in years:
         for mon in months:
-            df_temp = df_line[(df_line["year"]==y) & (df_line["month"]==mon)]
+            temp = df_line
+            df_temp = temp.loc[df_line['year'] == y]
+            df_temp = df_temp.loc[df_line['month'] == mon]
             total = df_temp["distance"].sum()
             temp_list = [y, mon, total]
             df_graph.loc[df_graph.shape[0]] = temp_list
-
-
-    template = "plotly_white"
-
-    # fig = px.scatter(df,
-    #                 x="month", y="distance", color="type",
-    #                 log_x=True, size_max=60,
-    #                 template=template, title="Strava Analysis")
-    # fig.show()
-
-    fig = px.scatter(df_graph, x="month", y="distance", color="year")
+    title = a_type + ": Distance per month, separated by year"
+    fig = px.scatter(df_graph, x="month", y="distance", color="year", color_discrete_map= {'2018': 'black','2019': '#f7d0b5','2020': "#FC6100",'2021': '#243856','2022': '#909090'})
+    
     fig.update_traces(marker=dict(size=20),
                   selector=dict(mode='markers'))
-    # graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
-    # fig.show()
+    fig.update_layout(title_text=title, title_x=0.5)
+
+    hr_speed_graph = hr_graph(df, years, a_type)
+    bar_fig = year_dist_graph(df_line, years, a_type)
+    heart_rate_graph = plot(hr_speed_graph, output_type='div')
+    bar_graph = plot(bar_fig, output_type='div')
     graph = plot(fig, output_type='div')
-    return graph
+    return graph, bar_graph, heart_rate_graph
+
+
+def year_dist_graph(df_line, years, a_type):
+    df_graph = pd.DataFrame(columns = ["year", "distance"])
+    for y in years:
+        temp = df_line
+        df_temp = temp.loc[df_line['year'] == y]
+        total = df_temp["distance"].sum()
+        temp_list = [y, total]
+        df_graph.loc[df_graph.shape[0]] = temp_list
+    title = a_type + ": Distance per year"
+    fig = px.bar(df_graph, x="distance", y="year", orientation='h', color="year", color_discrete_map= {'2018': 'black','2019': '#f7d0b5','2020': "#FC6100",'2021': '#243856','2022': '#909090'})
+    fig.update_layout(title_text=title, title_x=0.5)
+    return fig
+
+def hr_graph(df, years, a_type):
+    pio.templates.default = "plotly_white"
+    df_t = df[["year", "type", "average_heartrate", "average_speed"]]
+    if a_type != "All":
+        df_line = df_t[(df_t["type"] == a_type)]
+        df_line = speed_units(df_line, a_type)
+    else:
+        df_line = df_t
+    df_line = df_line.astype({"year": str})
+    df_line = df_line[df_line['year'].isin(years)]
+    # df_line = df_line[df_line.average_heartrate != 0]
+    df_line = df_line[df_line.average_speed != 0]
+    outliers = df_line.average_heartrate.quantile(0.32)
+    df_line = df_line[df_line.average_heartrate > outliers]
+    title = a_type + ": Average Speed to Average HR"
+    fig = px.scatter(df_line, x="average_speed", y="average_heartrate", color="year", color_discrete_map= {'2018': 'black','2019': '#f7d0b5','2020': "#FC6100",'2021': '#243856','2022': '#909090'})
+    fig.update_layout(xaxis_title="Avg Speed", yaxis_title="Avg HR", title_text=title, title_x=0.5)
+    return fig
+# TODO: fix! not accurately calculating pace... :(
+def speed_units(df_line, a_type):
+    if a_type == 'Run':
+        df_line["average_speed"] = df_line["average_speed"].apply(lambda x: x * (50/3))
+    return df_line
+
+def find_activity(id_1, id_2, activity_data):
+    activity_1 = 0
+    activity_2 = 0
+    for page in activity_data:
+        # iterate through each activity on page 
+        for i in page:
+            # checks to see if the type of activity matches the user's filter input
+            if i["id"]== id_1:
+                activity_1 = i
+            if i["id"] == id_2:
+                activity_2 = i
+    if activity_1 != 0 and activity_2 != 0:
+        if activity_1["end_latlng"] == activity_2["end_latlng"]:
+            result = "Correct"
+            return result
+        else:
+            result = "Not Correct"
+            return result
+            
+    else:
+        result = "The id value(s) entered are not valid. Please try again. Use the filtering page to find the correct activity ID."
+        return result    
+
 
 
 # List of TODOs:
@@ -235,3 +320,5 @@ def graph_data(year_list):
     # TODO: filter based on elevation
     # TODO: fix running pace, decimal places, right now they are in 100 -> convert to 60 (divide by 60)
     # TODO: find new gif for loading page, and fix formatting to maintain size of login form
+    # TODO: change join to merge instead (better wording)
+    # TODO: Avg pace per time of day --> graph
